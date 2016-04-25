b63e80815a8243087dc9e5860559bc040809b2f6
Fan Jiang <fanjiang@thoughtworks.com>
2016-04-25 16:35:35 -0500

    clean up some unnecessary stuff

--- ed448.go
+++ ed448.go
@@ -7,15 +7,15 @@ type Curve interface {
 	Params() *CurveParams
 	// IsOnCurve reports whether the given (x,y) lies on the curve.
 	IsOnCurve(x, y *big.Int) bool
-	// Add returns the sum of (x1,y1) and (x2,y2)
-	Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int)
-	// Double returns 2*(x,y)
-	Double(x1, y1 *big.Int) (x, y *big.Int)
-	// ScalarMult returns k*(Bx,By) where k is a number in big-endian form.
-	ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int)
-	// ScalarBaseMult returns k*G, where G is the base point of the group
-	// and k is an integer in big-endian form.
-	ScalarBaseMult(k []byte) (x, y *big.Int)
+	// // Add returns the sum of (x1,y1) and (x2,y2)
+	// Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int)
+	// // Double returns 2*(x,y)
+	// Double(x1, y1 *big.Int) (x, y *big.Int)
+	// // ScalarMult returns k*(Bx,By) where k is a number in big-endian form.
+	// ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int)
+	// // ScalarBaseMult returns k*G, where G is the base point of the group
+	// // and k is an integer in big-endian form.
+	// ScalarBaseMult(k []byte) (x, y *big.Int)
 }
 
 type CurveParams struct {
@@ -27,11 +27,11 @@ type CurveParams struct {
 	Name    string   // the canonical name of the curve
 }
 
-type Ed448Curve struct {
+type ed448Curve struct {
 	*CurveParams
 }
 
-var ed448 Ed448Curve
+var ed448 ed448Curve
 
 func init() {
 	ed448.CurveParams = &CurveParams{Name: "P-224"}
@@ -41,10 +41,10 @@ func init() {
 	ed448.Gx, _ = new(big.Int).SetString("297ea0ea2692ff1b4faff46098453a6a26adf733245f065c3c59d0709cecfa96147eaaf3932d94c63d96c170033f4ba0c7f0de840aed939f", 16)
 	ed448.Gy, _ = new(big.Int).SetString("13", 16)
 	ed448.BitSize = 448
+}
 
-	// p224FromBig(&p224.gx, p224.Gx)
-	// p224FromBig(&p224.gy, p224.Gy)
-	// p224FromBig(&p224.b, p224.B)
+func Ed448() Curve {
+	return ed448
 }
 
 func (curve *CurveParams) Params() *CurveParams {
