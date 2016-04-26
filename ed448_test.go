a0bd5e7453e7735299d77a8700c866e5f3eb3d4d
Fan Jiang <fanjiang@thoughtworks.com>
2016-04-26 15:24:17 -0500

    an optimized double

--- ed448_test.go
+++ ed448_test.go
@@ -8,3 +8,12 @@ func TestOnCurve(t *testing.T) {
 		t.Errorf("FAIL")
 	}
 }
+
+func TestDouble(t *testing.T) {
+	ed448 := Ed448()
+	x2, y2 := ed448.Double(ed448.Params().Gx, ed448.Params().Gy)
+	x3, y3 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
+	if x2.Cmp(x3) != 0 || y2.Cmp(y3) != 0 {
+		t.Errorf("FAIL")
+	}
+}
