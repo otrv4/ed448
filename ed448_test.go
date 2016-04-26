3117ede8237a780f26d973c4c61987e789d70f99
Fan Jiang <fanjiang@thoughtworks.com>
2016-04-26 17:09:32 -0500

    revert the changes of Ed448Curve construction

--- ed448_test.go
+++ ed448_test.go
@@ -14,6 +14,6 @@ func TestDouble(t *testing.T) {
 	x2, y2 := ed448.Double(ed448.Params().Gx, ed448.Params().Gy)
 	x3, y3 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
 	if x2.Cmp(x3) != 0 || y2.Cmp(y3) != 0 {
-		t.Errorf("FAIL")
+		t.Errorf("%v %v", y2, y3)
 	}
 }
