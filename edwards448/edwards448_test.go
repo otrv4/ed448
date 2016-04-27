05a6dfa49eb4ae4b82abc1a200b0bf97fead8a23
Iván Pazmiño <iapazmino@gmail.com>
2016-04-27 10:42:19 -0500

    Added some assertion helpers.

--- edwards448/edwards448_test.go
+++ edwards448/edwards448_test.go
@@ -4,17 +4,13 @@ import "testing"
 
 func TestOnCurve(t *testing.T) {
 	ed448 := Ed448()
-	if !ed448.IsOnCurve(ed448.Params().Gx, ed448.Params().Gy) {
-		t.Errorf("ed448 base point is not on curve")
-	}
+	assert_true(t, ed448.IsOnCurve(ed448.Params().Gx, ed448.Params().Gy))
 }
 
 func TestDouble(t *testing.T) {
 	ed448 := Ed448()
 	x2, y2 := ed448.Double(ed448.Params().Gx, ed448.Params().Gy)
 	x3, y3 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
-	if x2.Cmp(x3) != 0 || y2.Cmp(y3) != 0 {
-		t.Errorf("ed448 (Gx,Gy)+(Gx,Gy) is not equal to Double(Gx,Gy)")
-		t.Errorf("(%v, %v) != (%v, %v)", x2, y2, x3, y3)
-	}
+	assert_equals(t, x2, x3)
+	assert_equals(t, y2, y3)
 }
