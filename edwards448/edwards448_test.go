4a1cd3f4b3fdc8e37808c63cc57019f095808f9d
Fan Jiang <fanjiang@thoughtworks.com>
2016-04-28 16:51:07 -0500

    use better names for tests

--- edwards448/edwards448_test.go
+++ edwards448/edwards448_test.go
@@ -9,16 +9,22 @@ func TestOnCurve(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	ed448 := Ed448()
-	x3, y3 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
-	assert_true(t, ed448.IsOnCurve(x3, y3))
-	x4, y4 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, x3, y3)
+	x2, y2 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
+	assert_true(t, ed448.IsOnCurve(x2, y2))
+	x4, y4 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, x2, y2)
 	assert_true(t, ed448.IsOnCurve(x4, y4))
 }
 
 func TestDouble(t *testing.T) {
 	ed448 := Ed448()
-	x2, y2 := ed448.Double(ed448.Params().Gx, ed448.Params().Gy)
-	x3, y3 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
-	assert_equals(t, x2, x3)
-	assert_equals(t, y2, y3)
+	xd2, yd2 := ed448.Double(ed448.Params().Gx, ed448.Params().Gy)
+	xa2, ya2 := ed448.Add(ed448.Params().Gx, ed448.Params().Gy, ed448.Params().Gx, ed448.Params().Gy)
+	xd4, yd4 := ed448.Double(xd2, yd2)
+	xa4, ya4 := ed448.Add(xa2, ya2, xa2, ya2)
+	assert_equals(t, xd2, xa2)
+	assert_equals(t, yd2, ya2)
+	assert_equals(t, xd4, xa4)
+	assert_equals(t, yd4, ya4)
+	assert_true(t, ed448.IsOnCurve(xd2, yd2))
+	assert_true(t, ed448.IsOnCurve(xd4, yd4))
 }
