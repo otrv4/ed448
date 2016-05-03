b0f0d66c6b0041928ea5164938bd0a55d71d2e9c
Iván Pazmiño <iapazmino@gmail.com>
2016-05-03 11:38:24 -0500

    Curve is a internal struct without interface.

--- ed448.go
+++ ed448.go
@@ -10,7 +10,7 @@ var mask = []byte{0xff, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, 0x7f}
 // GenerateKey returns a public/private key pair. The private key is
 // generated using the given reader, which must return random data.
 func GenerateKey(curve curve, rand io.Reader) (priv []byte, pub []byte, err error) {
-	N := curve.Params().N
+	N := curve.N
 	bitSize := N.BitLen()
 	byteLen := (bitSize + 7) >> 3
 	priv = make([]byte, byteLen)
@@ -43,7 +43,7 @@ func GenerateKey(curve curve, rand io.Reader) (priv []byte, pub []byte, err erro
 
 // Marshal converts a point into the form specified in section 4.3.6 of ANSI X9.62.
 func Marshal(curve curve, x, y *big.Int) []byte {
-	byteLen := (curve.Params().BitSize + 7) >> 3
+	byteLen := (curve.BitSize + 7) >> 3
 
 	ret := make([]byte, 1+2*byteLen)
 	ret[0] = 4 // uncompressed point
@@ -58,7 +58,7 @@ func Marshal(curve curve, x, y *big.Int) []byte {
 // Unmarshal converts a point, serialized by Marshal, into an x, y pair.
 // It is an error if the point is not on the curve. On error, x = nil.
 func Unmarshal(curve curve, data []byte) (x, y *big.Int) {
-	byteLen := (curve.Params().BitSize + 7) >> 3
+	byteLen := (curve.BitSize + 7) >> 3
 	if len(data) != 1+2*byteLen {
 		return
 	}
