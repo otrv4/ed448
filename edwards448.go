8a2c1ddb853ea518235ff93456f4041feb6f4b7a
Iván Pazmiño <iapazmino@gmail.com>
2016-05-03 11:56:00 -0500

    Extracted regularly used big ints.

--- edwards448.go
+++ edwards448.go
@@ -3,7 +3,7 @@ package ed448
 import "math/big"
 
 type curve struct {
-	P       *big.Int // the order of the underlying field
+	p       *big.Int // the order of the underlying field
 	N       *big.Int // the order of the base point
 	B       *big.Int // the constant of the curve equation
 	Gx, Gy  *big.Int // (x,y) of the base point
@@ -11,14 +11,21 @@ type curve struct {
 }
 
 var ed448 curve
+var zero, one, two *big.Int
 
 func init() {
-	ed448.P, _ = new(big.Int).SetString("fffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16)
+	ed448.p, _ = new(big.Int).SetString("fffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16)
 	ed448.N, _ = new(big.Int).SetString("3fffffffffffffffffffffffffffffffffffffffffffffffffffffff7cca23e9c44edb49aed63690216cc2728dc58f552378c292ab5844f3", 16)
 	ed448.B, _ = new(big.Int).SetString("-39081", 10)
 	ed448.Gx, _ = new(big.Int).SetString("297ea0ea2692ff1b4faff46098453a6a26adf733245f065c3c59d0709cecfa96147eaaf3932d94c63d96c170033f4ba0c7f0de840aed939f", 16)
 	ed448.Gy, _ = new(big.Int).SetString("13", 16)
 	ed448.BitSize = 448
+}
+
+func init() {
+	zero = big.NewInt(0)
+	one = big.NewInt(1)
+	two = big.NewInt(2)
 }
 
 func newEd448() curve {
@@ -35,9 +42,9 @@ func (c *curve) IsOnCurve(x, y *big.Int) bool {
 	bx2y2 := mul(c.B, x2y2)
 
 	left := add(x2, y2)
-	left = mod(left, c.P)
-	right := add(one(), bx2y2)
-	right = mod(right, c.P)
+	left = mod(left)
+	right := add(one, bx2y2)
+	right = mod(right)
 
 	return left.Cmp(right) == 0
 }
@@ -49,18 +56,18 @@ func (c *curve) Add(x1, y1, x2, y2 *big.Int) (x3, y3 *big.Int) {
 	// y3 =  y1y2 - x1x2 / 1 - bx1x2y1y2
 
 	bx1x2y1y2 := mul(c.B, mul(x1, mul(x2, mul(y1, y2))))
-	bx1x2y1y2 = mod(bx1x2y1y2, c.P)
+	bx1x2y1y2 = mod(bx1x2y1y2)
 
 	x3 = mul(x1, y2)
 	x3 = add(x3, mul(x2, y1))
-	x3 = mod(x3, c.P)
-	divisor := modInv(mod(add(one(), bx1x2y1y2), c.P), c.P)
+	x3 = mod(x3)
+	divisor := modInv(mod(add(one, bx1x2y1y2)))
 	x3 = mul(x3, divisor)
 
 	y3 = mul(y1, y2)
 	y3 = sub(y3, mul(x1, x2))
-	y3 = mod(y3, c.P)
-	divisor = modInv(mod(sub(one(), bx1x2y1y2), c.P), c.P)
+	y3 = mod(y3)
+	divisor = modInv(mod(sub(one, bx1x2y1y2)))
 	y3 = mul(y3, divisor)
 
 	return
@@ -73,17 +80,17 @@ func (c *curve) Double(x1, y1 *big.Int) (x3, y3 *big.Int) {
 	// y3 =  y² - x² / 1 - bx²y² = y² - x² / 2 - x² - y²
 
 	x2plusy2 := add(mul(x1, x1), mul(y1, y1))
-	x2plusy2 = mod(x2plusy2, c.P)
+	x2plusy2 = mod(x2plusy2)
 
 	x3 = mul(x1, y1)
 	x3.Lsh(x3, 1) // x3 = 2xy
-	x3 = mod(x3, c.P)
-	divisor := modInv(x2plusy2, c.P)
+	x3 = mod(x3)
+	divisor := modInv(x2plusy2)
 	x3 = mul(x3, divisor) // x3 = 2xy / x² + y²
 
 	y3 = sub(mul(y1, y1), mul(x1, x1)) // y3 = y² - x²
-	y3 = mod(y3, c.P)
-	divisor = modInv(mod(sub(two(), x2plusy2), c.P), c.P)
+	y3 = mod(y3)
+	divisor = modInv(mod(sub(two, x2plusy2)))
 	y3 = mul(y3, divisor) // y3 = y² - x² / 2 - x² - y²
 
 	return
@@ -94,13 +101,13 @@ func (c *curve) Multiply(x, y *big.Int, k []byte) (kx, ky *big.Int) {
 	kx, ky = x, y
 	n := new(big.Int).SetBytes(k)
 
-	for n.Cmp(zero()) > 0 {
-		if mod(n, two()).Cmp(zero()) == 0 {
+	for n.Cmp(zero) > 0 {
+		if new(big.Int).Mod(n, two).Cmp(zero) == 0 {
 			kx, ky = c.Double(kx, ky)
-			n = sub(n, two())
+			n = sub(n, two)
 		} else {
 			kx, ky = c.Add(kx, ky, x, y)
-			n = sub(n, one())
+			n = sub(n, one)
 		}
 	}
 	return
@@ -128,22 +135,10 @@ func square(v *big.Int) *big.Int {
 	return new(big.Int).Mul(v, v)
 }
 
-func mod(x, y *big.Int) *big.Int {
-	return new(big.Int).Mod(x, y)
+func mod(x *big.Int) *big.Int {
+	return new(big.Int).Mod(x, newEd448().p)
 }
 
-func modInv(x, y *big.Int) *big.Int {
-	return new(big.Int).ModInverse(x, y)
-}
-
-func zero() *big.Int {
-	return big.NewInt(0)
-}
-
-func one() *big.Int {
-	return big.NewInt(1)
-}
-
-func two() *big.Int {
-	return big.NewInt(2)
+func modInv(x *big.Int) *big.Int {
+	return new(big.Int).ModInverse(x, newEd448().p)
 }
