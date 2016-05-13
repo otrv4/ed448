48d6bade04c7e6e185062d3ea00ed714bf281e99
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-05-13 17:57:50 -0500

    Serialize array of bytes into big numbers

--- arithmetic.go
+++ arithmetic.go
@@ -1,15 +1,21 @@
 package ed448
 
+const (
+	radix = 56
+	limbs = 8
+)
+
 type word uint64
+type bigNumber [limbs]word
 
 func isZero(n int64) int64 {
 	return ^n
 }
 
-func constantTimeGreaterOrEqualP(n [8]int64) bool {
+func constantTimeGreaterOrEqualP(n [limbs]int64) bool {
 	var (
 		ge   = int64(-1)
-		mask = int64(1)<<56 - 1
+		mask = int64(1)<<radix - 1
 	)
 
 	for i := 0; i < 4; i++ {
@@ -23,4 +29,21 @@ func constantTimeGreaterOrEqualP(n [8]int64) bool {
 	}
 
 	return ge == mask
+}
+
+func serialize(dst []byte, src bigNumber) {
+	const (
+		rows    = limbs
+		columns = radix / limbs
+	)
+
+	var n bigNumber
+	copy(n[:], src[:])
+
+	for i := uint(0); i < rows; i++ {
+		for j := uint(0); j < columns; j++ {
+			dst[columns*i+j] = byte(n[i])
+			n[i] >>= 8
+		}
+	}
 }
