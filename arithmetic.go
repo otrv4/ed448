a7f061547a6032d43e2417d496c210b81070323a
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-05-16 14:19:19 -0500

    Add operations on Words
    I believe they are architecture-independent

--- arithmetic.go
+++ arithmetic.go
@@ -67,3 +67,100 @@ func serialize(dst []byte, src bigNumber) {
 		}
 	}
 }
+
+/*This is adapted from https://golang.org/src/math/big/arith.go */
+type Word uintptr
+
+const (
+	// Compute the size _S of a Word in bytes.
+	_m    = ^Word(0)
+	_logS = _m>>8&1 + _m>>16&1 + _m>>32&1
+	_S    = 1 << _logS
+
+	_W = _S << 3 // word size in bits
+	_B = 1 << _W // digit base
+	_M = _B - 1  // digit mask
+
+	_W2 = _W / 2   // half word size in bits
+	_B2 = 1 << _W2 // half digit base
+	_M2 = _B2 - 1  // half digit mask
+)
+
+// z1<<_W + z0 = x*y
+// Adapted from Warren, Hacker's Delight, p. 132.
+func mulWW_g(x, y Word) (z1, z0 Word) {
+	x0 := x & _M2
+	x1 := x >> _W2
+	y0 := y & _M2
+	y1 := y >> _W2
+	w0 := x0 * y0
+	t := x1*y0 + w0>>_W2
+	w1 := t & _M2
+	w2 := t >> _W2
+	w1 += x0 * y1
+	z1 = x1*y1 + w2 + w1>>_W2
+	z0 = x * y
+	return
+}
+
+// z1<<_W + z0 = x*y + c
+func mulAddWWW_g(x, y, c Word) (z1, z0 Word) {
+	z1, zz0 := mulWW_g(x, y)
+	if z0 = zz0 + c; z0 < zz0 {
+		z1++
+	}
+	return
+}
+
+// z1<<_W + z0 = x-y-c, with c == 0 or 1
+func subWW_g(x, y, c Word) (z1, z0 Word) {
+	yc := y + c
+	z0 = x - yc
+	if z0 > x || yc < y {
+		z1 = 1
+	}
+	return
+}
+
+type DWord struct {
+	h Word
+	l Word
+}
+
+func WideMul(x, y Word) DWord {
+	z1, z0 := mulWW_g(x, y)
+	return DWord{h: z1, l: z0}
+}
+
+func Mac(x, y Word, acc DWord) DWord {
+	z1, z0 := mulAddWWW_g(x, y, acc.l)
+
+	z11 := z1 + acc.h
+	if z11 < z1 {
+		panic("high word overflow")
+	}
+
+	return DWord{h: z11, l: z0}
+}
+
+func Msb(a, b Word, acc DWord) (z DWord, c Word) {
+	d := WideMul(a, b)
+
+	xi := d.l
+	yi := acc.l
+	zi := xi - yi
+
+	z.l = zi
+	// see "Hacker's Delight", section 2-12 (overflow detection)
+	c = (yi&^xi | (yi|^xi)&zi) >> (_W - 1)
+
+	xi = d.h
+	yi = acc.h
+	zi = xi - yi - c
+
+	z.h = zi
+	// see "Hacker's Delight", section 2-12 (overflow detection)
+	c = (yi&^xi | (yi|^xi)&zi) >> (_W - 1)
+
+	return
+}
--- arithmetic_test.go
+++ arithmetic_test.go
@@ -83,3 +83,29 @@ func (s *Ed448Suite) TestSerialize(c *C) {
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 	})
 }
+
+func (s *Ed448Suite) TestWordMult(c *C) {
+	//No carry on multiplication
+	result := WideMul(Word(0x01), Word(0x02))
+	c.Assert(result, Equals, DWord{0, 0x02})
+
+	//With carry on multiplication
+	result = WideMul(Word(0xffffffffffffffff), Word(0x02))
+	c.Assert(result, Equals, DWord{0x01, 0xfffffffffffffffe})
+
+	//No carry on addition
+	result = Mac(Word(0xffffffffffffffff), Word(0x02), DWord{0, 0x01})
+	c.Assert(result, Equals, DWord{0x01, 0xffffffffffffffff})
+
+	//With carry on addition
+	result = Mac(Word(0xffffffffffffffff), Word(0x02), DWord{0, 0x02})
+	c.Assert(result, Equals, DWord{0x02, 0x00})
+
+	//No borrow
+	result, _ = Msb(Word(0xffffffffffffffff), Word(0x02), DWord{0, 0x01})
+	c.Assert(result, Equals, DWord{0x01, 0xfffffffffffffffd})
+
+	//With borrow
+	result, _ = Msb(Word(0x8000000000000000), Word(0x02), DWord{0, 0x01})
+	c.Assert(result, Equals, DWord{0x00, 0xffffffffffffffff})
+}
