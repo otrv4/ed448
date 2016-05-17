0903dc0253bcbef0ea149ead677be5accd221b17
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-05-17 10:31:53 -0500

    Serialization is specific to architecture

--- arithmetic.go
+++ arithmetic.go
@@ -91,7 +91,7 @@ func addDWord(a, b DWord) (z DWord) {
 
 type limb Word
 type bigNumber [Limbs]limb
-type serialized [Radix]byte
+type serialized [56]byte
 
 func mustDeserialize(in serialized) bigNumber {
 	n, ok := deserialize(in)
@@ -100,62 +100,4 @@ func mustDeserialize(in serialized) bigNumber {
 	}
 
 	return n
-}
-
-func deserialize(in serialized) (n bigNumber, ok bool) {
-	const (
-		columns = Limbs
-		rows    = Limbs - 1
-	)
-
-	for i := uint(0); i < columns; i++ {
-		for j := uint(0); j < rows; j++ {
-			n[i] |= limb(in[rows*i+j]) << (columns * j)
-		}
-	}
-
-	ok = !constantTimeGreaterOrEqualP(n)
-
-	return
-}
-
-//TODO: Make this work with a word parameter
-func isZero(n int64) int64 {
-	return ^n
-}
-
-func constantTimeGreaterOrEqualP(n bigNumber) bool {
-	var (
-		ge   = int64(-1)
-		mask = int64(1)<<Radix - 1
-	)
-
-	for i := 0; i < 4; i++ {
-		ge &= int64(n[i])
-	}
-
-	ge = (ge & (int64(n[4]) + 1)) | isZero(int64(n[4])^mask)
-
-	for i := 5; i < 8; i++ {
-		ge &= int64(n[i])
-	}
-
-	return ge == mask
-}
-
-func serialize(dst []byte, src bigNumber) {
-	const (
-		rows    = Limbs
-		columns = Radix / Limbs
-	)
-
-	var n bigNumber
-	copy(n[:], src[:])
-
-	for i := uint(0); i < rows; i++ {
-		for j := uint(0); j < columns; j++ {
-			dst[columns*i+j] = byte(n[i])
-			n[i] >>= 8
-		}
-	}
 }
