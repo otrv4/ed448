7950b574065691f6cf1004e18a85fae76f234423
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-05-17 18:05:17 -0500

    Implement strongReduce

--- radix_386.go
+++ radix_386.go
@@ -1,20 +1,19 @@
 package ed448
 
 const (
-	Limbs = 16
-	Radix = 28
+	Limbs     = 16
+	Radix     = 28
+	radixMask = limb(0xfffffff)
 )
 
 func deserialize(in serialized) (n bigNumber, ok bool) {
-	mask := limb(0xfffffff)
-
 	for i := uint(0); i < 8; i++ {
 		out := uint64(0)
 		for j := uint(0); j < 7; j++ {
 			out |= uint64(in[7*i+j]) << (8 * j)
 		}
 
-		n[2*i] = limb(out) & mask
+		n[2*i] = limb(out) & radixMask
 		n[2*i+1] = limb(out >> 28)
 	}
 
@@ -22,8 +21,9 @@ func deserialize(in serialized) (n bigNumber, ok bool) {
 	return
 }
 
-func serialize(dst []byte, src bigNumber) {
-	//TODO strong reduce
+func serialize(dst []byte, n bigNumber) {
+	src := n.copy()
+	src.strongReduce()
 
 	for i := 0; i < 8; i++ {
 		l := uint64(src[2*i]) + uint64(src[2*i+1])<<28
@@ -32,6 +32,43 @@ func serialize(dst []byte, src bigNumber) {
 			l >>= 8
 		}
 	}
+}
 
-	//TODO
+func (n *bigNumber) copy() *bigNumber {
+	c := &bigNumber{}
+	copy(c[:], n[:])
+	return c
+}
+
+func (n *bigNumber) strongReduce() {
+	// clear high
+	n[8] += n[15] >> 28
+	n[0] += n[15] >> 28
+	n[15] &= radixMask
+
+	scarry := int64(0)
+	for i := 0; i < 16; i++ {
+		m := uint32(radixMask)
+		if i == 8 {
+			m = uint32(0xffffffe)
+		}
+
+		scarry += int64(n[i]) - int64(m)
+
+		n[i] = limb(scarry) & radixMask
+		scarry >>= 28
+	}
+
+	scarryMask := Word(scarry) & Word(radixMask)
+	carry := uint64(0)
+	for i := 0; i < 16; i++ {
+		m := uint64(scarryMask)
+		if i == 8 {
+			m &= uint64(0xfffffffffffffffe)
+		}
+
+		carry += uint64(n[i]) + m
+		n[i] = limb(carry) & radixMask
+		carry >>= 28
+	}
 }
