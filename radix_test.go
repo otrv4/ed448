ee8fddf308d195373bf4b89177304d637f45c3f7
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-06-03 01:16:30 -0500

    Add test for negate

---
+++ radix_test.go
@@ -0,0 +1,18 @@
+package ed448
+
+import (
+	"encoding/hex"
+
+	. "gopkg.in/check.v1"
+)
+
+func (s *Ed448Suite) TestNegate(c *C) {
+	bs, _ := hex.DecodeString("e6f5b8ae49cef779e577dc29824eff453f1c4106030088115ea49b4ee84a7b7cdfe06e0d622fc55c7c559ab1f6c3ea3257c07979809026de")
+	n := new(bigNumber).setBytes(bs)
+	out := new(bigNumber).neg(n)
+
+	bs, _ = hex.DecodeString("190a4751b63108861a8823d67db100bac0e3bef9fcff77eea15b64b017b58483201f91f29dd03aa383aa654e093c15cda83f86867f6fd921")
+	expected := new(bigNumber).setBytes(bs)
+
+	c.Assert(out, DeepEquals, expected)
+}
