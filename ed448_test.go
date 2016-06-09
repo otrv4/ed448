2bfdc763efa2fc0085c364cc7b9852093a3e4f90
Iván Pazmiño <iapazmino@gmail.com>
2016-06-09 17:19:13 -0500

    Defined exported functions.

--- ed448_test.go
+++ ed448_test.go
@@ -12,11 +12,27 @@ type Ed448Suite struct{}
 
 var _ = Suite(&Ed448Suite{})
 
-func (s *Ed448Suite) TestKeysGenerationProducesKeyPair(c *C) {
+func (s *Ed448Suite) TestGenerateKeysProducesKeyPair(c *C) {
 	c.Skip("Public key is not being set yet.")
 	ed448 := NewEd448()
 	priv, pub, err := ed448.GenerateKeys()
 	c.Assert(err, IsNil)
 	c.Assert(priv, NotNil)
 	c.Assert(pub, NotNil)
+}
+
+func (s *Ed448Suite) TestSignAndVerify(c *C) {
+	c.Skip("Public key is not being set yet.")
+	ed448 := NewEd448()
+	priv, pub, _ := ed448.GenerateKeys()
+	message := []byte("sign here.")
+
+	signature, err := ed448.Sign(priv, message)
+
+	c.Assert(err, IsNil)
+	c.Assert(signature, NotNil)
+
+	valid := ed448.Verify(signature, message, pub)
+
+	c.Assert(valid, Equals, true)
 }
