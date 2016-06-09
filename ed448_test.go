3dd86e951b58ec9bb6479e0aeccd3c7baeee1c9e
Iván Pazmiño <iapazmino@gmail.com>
2016-06-09 16:56:48 -0500

    Key generation exported.

--- ed448_test.go
+++ ed448_test.go
@@ -11,3 +11,12 @@ func Test(t *testing.T) { TestingT(t) }
 type Ed448Suite struct{}
 
 var _ = Suite(&Ed448Suite{})
+
+func (s *Ed448Suite) TestKeyGenerationProducesValidPoint(c *C) {
+	c.Skip("Public key is not being set yet.")
+	ed448 := NewEd448()
+	priv, pub, err := ed448.GenerateKeys()
+	c.Assert(err, IsNil)
+	c.Assert(priv, NotNil)
+	c.Assert(pub, NotNil)
+}
