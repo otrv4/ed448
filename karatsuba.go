257de1c82cfe76b31f86705231c95f1ed4a8fe2c
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-06-24 18:46:31 -0500

    Reorganizes karatsubaMultiplication

--- karatsuba.go
+++ karatsuba.go
@@ -2,394 +2,349 @@ package ed448
 
 //c = a * b
 func karatsubaMul(c, a, b *bigNumber) *bigNumber {
-	var aa, bb [8]limb
+	var aa, bb [8]uint64
 	for i := 0; i < 8; i++ {
-		aa[i] = a[i] + a[i+8]
-		bb[i] = b[i] + b[i+8]
+		aa[i] = uint64(a[i]) + uint64(a[i+8])
+		bb[i] = uint64(b[i]) + uint64(b[i+8])
 	}
 
 	var z0, z1, z2 uint64
 
 	//j = 0
 	z2 = 0
-
 	z2 += uint64(a[0]) * uint64(b[0])
-	z1 += uint64(aa[0]) * uint64(bb[0])
-	z0 += uint64(a[8+0]) * uint64(b[8])
-
+	z1 += aa[0] * bb[0]
 	z1 -= z2
+	z0 += uint64(a[8]) * uint64(b[8])
 	z0 += z2
+
 	z2 = 0
+	z2 += aa[7] * bb[1]
+	z2 += aa[6] * bb[2]
+	z2 += aa[5] * bb[3]
+	z2 += aa[4] * bb[4]
+	z2 += aa[3] * bb[5]
+	z2 += aa[2] * bb[6]
+	z2 += aa[1] * bb[7]
 
-	z0 -= uint64(a[8+0-1]) * uint64(b[1])
-	z2 += uint64(aa[8+0-1]) * uint64(bb[1])
-	z1 += uint64(a[16+0-1]) * uint64(b[8+1])
-
-	z0 -= uint64(a[8+0-2]) * uint64(b[2])
-	z2 += uint64(aa[8+0-2]) * uint64(bb[2])
-	z1 += uint64(a[16+0-2]) * uint64(b[8+2])
-
-	z0 -= uint64(a[8+0-3]) * uint64(b[3])
-	z2 += uint64(aa[8+0-3]) * uint64(bb[3])
-	z1 += uint64(a[16+0-3]) * uint64(b[8+3])
-
-	z0 -= uint64(a[8+0-4]) * uint64(b[4])
-	z2 += uint64(aa[8+0-4]) * uint64(bb[4])
-	z1 += uint64(a[16+0-4]) * uint64(b[8+4])
-
-	z0 -= uint64(a[8+0-5]) * uint64(b[5])
-	z2 += uint64(aa[8+0-5]) * uint64(bb[5])
-	z1 += uint64(a[16+0-5]) * uint64(b[8+5])
-
-	z0 -= uint64(a[8+0-6]) * uint64(b[6])
-	z2 += uint64(aa[8+0-6]) * uint64(bb[6])
-	z1 += uint64(a[16+0-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+0-7]) * uint64(b[7])
-	z2 += uint64(aa[8+0-7]) * uint64(bb[7])
-	z1 += uint64(a[16+0-7]) * uint64(b[8+7])
-
+	z1 += uint64(a[15]) * uint64(b[9])
+	z1 += uint64(a[14]) * uint64(b[10])
+	z1 += uint64(a[13]) * uint64(b[11])
+	z1 += uint64(a[12]) * uint64(b[12])
+	z1 += uint64(a[11]) * uint64(b[13])
+	z1 += uint64(a[10]) * uint64(b[14])
+	z1 += uint64(a[9]) * uint64(b[15])
 	z1 += z2
+
+	z0 -= uint64(a[7]) * uint64(b[1])
+	z0 -= uint64(a[6]) * uint64(b[2])
+	z0 -= uint64(a[5]) * uint64(b[3])
+	z0 -= uint64(a[4]) * uint64(b[4])
+	z0 -= uint64(a[3]) * uint64(b[5])
+	z0 -= uint64(a[2]) * uint64(b[6])
+	z0 -= uint64(a[1]) * uint64(b[7])
 	z0 += z2
 
 	c[0] = limb(z0) & radixMask
-	c[0+8] = limb(z1) & radixMask
+	c[8] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 1
 	z2 = 0
-
 	z2 += uint64(a[1]) * uint64(b[0])
-	z1 += uint64(aa[1]) * uint64(bb[0])
-	z0 += uint64(a[8+1]) * uint64(b[8])
+	z2 += uint64(a[0]) * uint64(b[1])
 
-	z2 += uint64(a[1-1]) * uint64(b[1])
-	z1 += uint64(aa[1-1]) * uint64(bb[1])
-	z0 += uint64(a[8+1-1]) * uint64(b[8+1])
-
+	z1 += aa[1] * bb[0]
+	z1 += aa[0] * bb[1]
 	z1 -= z2
+
+	z0 += uint64(a[9]) * uint64(b[8])
+	z0 += uint64(a[8]) * uint64(b[9])
 	z0 += z2
+
 	z2 = 0
 
-	z0 -= uint64(a[8+1-2]) * uint64(b[2])
-	z2 += uint64(aa[8+1-2]) * uint64(bb[2])
-	z1 += uint64(a[16+1-2]) * uint64(b[8+2])
+	z2 += aa[7] * bb[2]
+	z2 += aa[6] * bb[3]
+	z2 += aa[5] * bb[4]
+	z2 += aa[4] * bb[5]
+	z2 += aa[3] * bb[6]
+	z2 += aa[2] * bb[7]
 
-	z0 -= uint64(a[8+1-3]) * uint64(b[3])
-	z2 += uint64(aa[8+1-3]) * uint64(bb[3])
-	z1 += uint64(a[16+1-3]) * uint64(b[8+3])
-
-	z0 -= uint64(a[8+1-4]) * uint64(b[4])
-	z2 += uint64(aa[8+1-4]) * uint64(bb[4])
-	z1 += uint64(a[16+1-4]) * uint64(b[8+4])
-
-	z0 -= uint64(a[8+1-5]) * uint64(b[5])
-	z2 += uint64(aa[8+1-5]) * uint64(bb[5])
-	z1 += uint64(a[16+1-5]) * uint64(b[8+5])
-
-	z0 -= uint64(a[8+1-6]) * uint64(b[6])
-	z2 += uint64(aa[8+1-6]) * uint64(bb[6])
-	z1 += uint64(a[16+1-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+1-7]) * uint64(b[7])
-	z2 += uint64(aa[8+1-7]) * uint64(bb[7])
-	z1 += uint64(a[16+1-7]) * uint64(b[8+7])
-
+	z1 += uint64(a[15]) * uint64(b[10])
+	z1 += uint64(a[14]) * uint64(b[11])
+	z1 += uint64(a[13]) * uint64(b[12])
+	z1 += uint64(a[12]) * uint64(b[13])
+	z1 += uint64(a[11]) * uint64(b[14])
+	z1 += uint64(a[10]) * uint64(b[15])
 	z1 += z2
+
+	z0 -= uint64(a[7]) * uint64(b[2])
+	z0 -= uint64(a[6]) * uint64(b[3])
+	z0 -= uint64(a[5]) * uint64(b[4])
+	z0 -= uint64(a[4]) * uint64(b[5])
+	z0 -= uint64(a[3]) * uint64(b[6])
+	z0 -= uint64(a[2]) * uint64(b[7])
 	z0 += z2
 
 	c[1] = limb(z0) & radixMask
-	c[1+8] = limb(z1) & radixMask
+	c[9] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 2
 	z2 = 0
-
 	z2 += uint64(a[2]) * uint64(b[0])
-	z1 += uint64(aa[2]) * uint64(bb[0])
-	z0 += uint64(a[8+2]) * uint64(b[8])
+	z2 += uint64(a[1]) * uint64(b[1])
+	z2 += uint64(a[0]) * uint64(b[2])
 
-	z2 += uint64(a[2-1]) * uint64(b[1])
-	z1 += uint64(aa[2-1]) * uint64(bb[1])
-	z0 += uint64(a[8+2-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[2-2]) * uint64(b[2])
-	z1 += uint64(aa[2-2]) * uint64(bb[2])
-	z0 += uint64(a[8+2-2]) * uint64(b[8+2])
-
+	z1 += aa[2] * bb[0]
+	z1 += aa[1] * bb[1]
+	z1 += aa[0] * bb[2]
 	z1 -= z2
+
+	z0 += uint64(a[10]) * uint64(b[8])
+	z0 += uint64(a[9]) * uint64(b[9])
+	z0 += uint64(a[8]) * uint64(b[10])
 	z0 += z2
+
 	z2 = 0
+	z2 += aa[7] * bb[3]
+	z2 += aa[6] * bb[4]
+	z2 += aa[5] * bb[5]
+	z2 += aa[4] * bb[6]
+	z2 += aa[3] * bb[7]
 
-	z0 -= uint64(a[8+2-3]) * uint64(b[3])
-	z2 += uint64(aa[8+2-3]) * uint64(bb[3])
-	z1 += uint64(a[16+2-3]) * uint64(b[8+3])
-
-	z0 -= uint64(a[8+2-4]) * uint64(b[4])
-	z2 += uint64(aa[8+2-4]) * uint64(bb[4])
-	z1 += uint64(a[16+2-4]) * uint64(b[8+4])
-
-	z0 -= uint64(a[8+2-5]) * uint64(b[5])
-	z2 += uint64(aa[8+2-5]) * uint64(bb[5])
-	z1 += uint64(a[16+2-5]) * uint64(b[8+5])
-
-	z0 -= uint64(a[8+2-6]) * uint64(b[6])
-	z2 += uint64(aa[8+2-6]) * uint64(bb[6])
-	z1 += uint64(a[16+2-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+2-7]) * uint64(b[7])
-	z2 += uint64(aa[8+2-7]) * uint64(bb[7])
-	z1 += uint64(a[16+2-7]) * uint64(b[8+7])
-
+	z1 += uint64(a[15]) * uint64(b[11])
+	z1 += uint64(a[14]) * uint64(b[12])
+	z1 += uint64(a[13]) * uint64(b[13])
+	z1 += uint64(a[12]) * uint64(b[14])
+	z1 += uint64(a[11]) * uint64(b[15])
 	z1 += z2
+
+	z0 -= uint64(a[7]) * uint64(b[3])
+	z0 -= uint64(a[6]) * uint64(b[4])
+	z0 -= uint64(a[5]) * uint64(b[5])
+	z0 -= uint64(a[4]) * uint64(b[6])
+	z0 -= uint64(a[3]) * uint64(b[7])
 	z0 += z2
 
 	c[2] = limb(z0) & radixMask
-	c[2+8] = limb(z1) & radixMask
+	c[10] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 3
 	z2 = 0
-
 	z2 += uint64(a[3]) * uint64(b[0])
-	z1 += uint64(aa[3]) * uint64(bb[0])
-	z0 += uint64(a[8+3]) * uint64(b[8])
+	z2 += uint64(a[2]) * uint64(b[1])
+	z2 += uint64(a[1]) * uint64(b[2])
+	z2 += uint64(a[0]) * uint64(b[3])
 
-	z2 += uint64(a[3-1]) * uint64(b[1])
-	z1 += uint64(aa[3-1]) * uint64(bb[1])
-	z0 += uint64(a[8+3-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[3-2]) * uint64(b[2])
-	z1 += uint64(aa[3-2]) * uint64(bb[2])
-	z0 += uint64(a[8+3-2]) * uint64(b[8+2])
-
-	z2 += uint64(a[3-3]) * uint64(b[3])
-	z1 += uint64(aa[3-3]) * uint64(bb[3])
-	z0 += uint64(a[8+3-3]) * uint64(b[8+3])
-
+	z1 += aa[3] * bb[0]
+	z1 += aa[2] * bb[1]
+	z1 += aa[1] * bb[2]
+	z1 += aa[0] * bb[3]
 	z1 -= z2
+
+	z0 += uint64(a[11]) * uint64(b[8])
+	z0 += uint64(a[10]) * uint64(b[9])
+	z0 += uint64(a[9]) * uint64(b[10])
+	z0 += uint64(a[8]) * uint64(b[11])
 	z0 += z2
+
 	z2 = 0
+	z2 += aa[7] * bb[4]
+	z2 += aa[6] * bb[5]
+	z2 += aa[5] * bb[6]
+	z2 += aa[4] * bb[7]
 
-	z0 -= uint64(a[8+3-4]) * uint64(b[4])
-	z2 += uint64(aa[8+3-4]) * uint64(bb[4])
-	z1 += uint64(a[16+3-4]) * uint64(b[8+4])
-
-	z0 -= uint64(a[8+3-5]) * uint64(b[5])
-	z2 += uint64(aa[8+3-5]) * uint64(bb[5])
-	z1 += uint64(a[16+3-5]) * uint64(b[8+5])
-
-	z0 -= uint64(a[8+3-6]) * uint64(b[6])
-	z2 += uint64(aa[8+3-6]) * uint64(bb[6])
-	z1 += uint64(a[16+3-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+3-7]) * uint64(b[7])
-	z2 += uint64(aa[8+3-7]) * uint64(bb[7])
-	z1 += uint64(a[16+3-7]) * uint64(b[8+7])
-
-	z1 += z2
+	z0 -= uint64(a[7]) * uint64(b[4])
+	z0 -= uint64(a[6]) * uint64(b[5])
+	z0 -= uint64(a[5]) * uint64(b[6])
+	z0 -= uint64(a[4]) * uint64(b[7])
 	z0 += z2
+
+	z1 += uint64(a[15]) * uint64(b[12])
+	z1 += uint64(a[14]) * uint64(b[13])
+	z1 += uint64(a[13]) * uint64(b[14])
+	z1 += uint64(a[12]) * uint64(b[15])
+	z1 += z2
 
 	c[3] = limb(z0) & radixMask
-	c[3+8] = limb(z1) & radixMask
+	c[11] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 4
 	z2 = 0
-
 	z2 += uint64(a[4]) * uint64(b[0])
-	z1 += uint64(aa[4]) * uint64(bb[0])
-	z0 += uint64(a[8+4]) * uint64(b[8])
+	z2 += uint64(a[3]) * uint64(b[1])
+	z2 += uint64(a[2]) * uint64(b[2])
+	z2 += uint64(a[1]) * uint64(b[3])
+	z2 += uint64(a[0]) * uint64(b[4])
 
-	z2 += uint64(a[4-1]) * uint64(b[1])
-	z1 += uint64(aa[4-1]) * uint64(bb[1])
-	z0 += uint64(a[8+4-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[4-2]) * uint64(b[2])
-	z1 += uint64(aa[4-2]) * uint64(bb[2])
-	z0 += uint64(a[8+4-2]) * uint64(b[8+2])
-
-	z2 += uint64(a[4-3]) * uint64(b[3])
-	z1 += uint64(aa[4-3]) * uint64(bb[3])
-	z0 += uint64(a[8+4-3]) * uint64(b[8+3])
-
-	z2 += uint64(a[4-4]) * uint64(b[4])
-	z1 += uint64(aa[4-4]) * uint64(bb[4])
-	z0 += uint64(a[8+4-4]) * uint64(b[8+4])
-
+	z1 += aa[4] * bb[0]
+	z1 += aa[3] * bb[1]
+	z1 += aa[2] * bb[2]
+	z1 += aa[1] * bb[3]
+	z1 += aa[0] * bb[4]
 	z1 -= z2
+
+	z0 += uint64(a[12]) * uint64(b[8])
+	z0 += uint64(a[11]) * uint64(b[9])
+	z0 += uint64(a[10]) * uint64(b[10])
+	z0 += uint64(a[9]) * uint64(b[11])
+	z0 += uint64(a[8]) * uint64(b[12])
 	z0 += z2
+
 	z2 = 0
+	z2 += aa[7] * bb[5]
+	z2 += aa[6] * bb[6]
+	z2 += aa[5] * bb[7]
 
-	z0 -= uint64(a[8+4-5]) * uint64(b[5])
-	z2 += uint64(aa[8+4-5]) * uint64(bb[5])
-	z1 += uint64(a[16+4-5]) * uint64(b[8+5])
-
-	z0 -= uint64(a[8+4-6]) * uint64(b[6])
-	z2 += uint64(aa[8+4-6]) * uint64(bb[6])
-	z1 += uint64(a[16+4-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+4-7]) * uint64(b[7])
-	z2 += uint64(aa[8+4-7]) * uint64(bb[7])
-	z1 += uint64(a[16+4-7]) * uint64(b[8+7])
-
+	z1 += uint64(a[15]) * uint64(b[13])
+	z1 += uint64(a[14]) * uint64(b[14])
+	z1 += uint64(a[13]) * uint64(b[15])
 	z1 += z2
+
+	z0 -= uint64(a[7]) * uint64(b[5])
+	z0 -= uint64(a[6]) * uint64(b[6])
+	z0 -= uint64(a[5]) * uint64(b[7])
 	z0 += z2
 
 	c[4] = limb(z0) & radixMask
-	c[4+8] = limb(z1) & radixMask
+	c[12] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 5
 	z2 = 0
-
 	z2 += uint64(a[5]) * uint64(b[0])
-	z1 += uint64(aa[5]) * uint64(bb[0])
-	z0 += uint64(a[8+5]) * uint64(b[8])
+	z2 += uint64(a[4]) * uint64(b[1])
+	z2 += uint64(a[3]) * uint64(b[2])
+	z2 += uint64(a[2]) * uint64(b[3])
+	z2 += uint64(a[1]) * uint64(b[4])
+	z2 += uint64(a[0]) * uint64(b[5])
 
-	z2 += uint64(a[5-1]) * uint64(b[1])
-	z1 += uint64(aa[5-1]) * uint64(bb[1])
-	z0 += uint64(a[8+5-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[5-2]) * uint64(b[2])
-	z1 += uint64(aa[5-2]) * uint64(bb[2])
-	z0 += uint64(a[8+5-2]) * uint64(b[8+2])
-
-	z2 += uint64(a[5-3]) * uint64(b[3])
-	z1 += uint64(aa[5-3]) * uint64(bb[3])
-	z0 += uint64(a[8+5-3]) * uint64(b[8+3])
-
-	z2 += uint64(a[5-4]) * uint64(b[4])
-	z1 += uint64(aa[5-4]) * uint64(bb[4])
-	z0 += uint64(a[8+5-4]) * uint64(b[8+4])
-
-	z2 += uint64(a[5-5]) * uint64(b[5])
-	z1 += uint64(aa[5-5]) * uint64(bb[5])
-	z0 += uint64(a[8+5-5]) * uint64(b[8+5])
-
+	z1 += aa[5] * bb[0]
+	z1 += aa[4] * bb[1]
+	z1 += aa[3] * bb[2]
+	z1 += aa[2] * bb[3]
+	z1 += aa[1] * bb[4]
+	z1 += aa[0] * bb[5]
 	z1 -= z2
+
+	z0 += uint64(a[13]) * uint64(b[8])
+	z0 += uint64(a[12]) * uint64(b[9])
+	z0 += uint64(a[11]) * uint64(b[10])
+	z0 += uint64(a[10]) * uint64(b[11])
+	z0 += uint64(a[9]) * uint64(b[12])
+	z0 += uint64(a[8]) * uint64(b[13])
 	z0 += z2
+
 	z2 = 0
+	z2 += aa[7] * bb[6]
+	z2 += aa[6] * bb[7]
 
-	z0 -= uint64(a[8+5-6]) * uint64(b[6])
-	z2 += uint64(aa[8+5-6]) * uint64(bb[6])
-	z1 += uint64(a[16+5-6]) * uint64(b[8+6])
-
-	z0 -= uint64(a[8+5-7]) * uint64(b[7])
-	z2 += uint64(aa[8+5-7]) * uint64(bb[7])
-	z1 += uint64(a[16+5-7]) * uint64(b[8+7])
-
+	z1 += uint64(a[15]) * uint64(b[14])
+	z1 += uint64(a[14]) * uint64(b[15])
 	z1 += z2
+
+	z0 -= uint64(a[7]) * uint64(b[6])
+	z0 -= uint64(a[6]) * uint64(b[7])
 	z0 += z2
 
 	c[5] = limb(z0) & radixMask
-	c[5+8] = limb(z1) & radixMask
+	c[13] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 6
 	z2 = 0
-
 	z2 += uint64(a[6]) * uint64(b[0])
-	z1 += uint64(aa[6]) * uint64(bb[0])
-	z0 += uint64(a[8+6]) * uint64(b[8])
+	z2 += uint64(a[5]) * uint64(b[1])
+	z2 += uint64(a[4]) * uint64(b[2])
+	z2 += uint64(a[3]) * uint64(b[3])
+	z2 += uint64(a[2]) * uint64(b[4])
+	z2 += uint64(a[1]) * uint64(b[5])
+	z2 += uint64(a[0]) * uint64(b[6])
 
-	z2 += uint64(a[6-1]) * uint64(b[1])
-	z1 += uint64(aa[6-1]) * uint64(bb[1])
-	z0 += uint64(a[8+6-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[6-2]) * uint64(b[2])
-	z1 += uint64(aa[6-2]) * uint64(bb[2])
-	z0 += uint64(a[8+6-2]) * uint64(b[8+2])
-
-	z2 += uint64(a[6-3]) * uint64(b[3])
-	z1 += uint64(aa[6-3]) * uint64(bb[3])
-	z0 += uint64(a[8+6-3]) * uint64(b[8+3])
-
-	z2 += uint64(a[6-4]) * uint64(b[4])
-	z1 += uint64(aa[6-4]) * uint64(bb[4])
-	z0 += uint64(a[8+6-4]) * uint64(b[8+4])
-
-	z2 += uint64(a[6-5]) * uint64(b[5])
-	z1 += uint64(aa[6-5]) * uint64(bb[5])
-	z0 += uint64(a[8+6-5]) * uint64(b[8+5])
-
-	z2 += uint64(a[6-6]) * uint64(b[6])
-	z1 += uint64(aa[6-6]) * uint64(bb[6])
-	z0 += uint64(a[8+6-6]) * uint64(b[8+6])
-
+	z1 += aa[6] * bb[0]
+	z1 += aa[5] * bb[1]
+	z1 += aa[4] * bb[2]
+	z1 += aa[3] * bb[3]
+	z1 += aa[2] * bb[4]
+	z1 += aa[1] * bb[5]
+	z1 += aa[0] * bb[6]
 	z1 -= z2
+
+	z0 += uint64(a[14]) * uint64(b[8])
+	z0 += uint64(a[13]) * uint64(b[9])
+	z0 += uint64(a[12]) * uint64(b[10])
+	z0 += uint64(a[11]) * uint64(b[11])
+	z0 += uint64(a[10]) * uint64(b[12])
+	z0 += uint64(a[9]) * uint64(b[13])
+	z0 += uint64(a[8]) * uint64(b[14])
 	z0 += z2
+
 	z2 = 0
-
-	z0 -= uint64(a[8+6-7]) * uint64(b[7])
-	z2 += uint64(aa[8+6-7]) * uint64(bb[7])
-	z1 += uint64(a[16+6-7]) * uint64(b[8+7])
-
+	z2 += aa[7] * bb[7]
+	z1 += uint64(a[15]) * uint64(b[15])
 	z1 += z2
+	z0 -= uint64(a[7]) * uint64(b[7])
 	z0 += z2
 
 	c[6] = limb(z0) & radixMask
-	c[6+8] = limb(z1) & radixMask
+	c[14] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
 
 	//j = 7
 	z2 = 0
-
 	z2 += uint64(a[7]) * uint64(b[0])
-	z1 += uint64(aa[7]) * uint64(bb[0])
-	z0 += uint64(a[8+7]) * uint64(b[8])
+	z2 += uint64(a[6]) * uint64(b[1])
+	z2 += uint64(a[5]) * uint64(b[2])
+	z2 += uint64(a[4]) * uint64(b[3])
+	z2 += uint64(a[3]) * uint64(b[4])
+	z2 += uint64(a[2]) * uint64(b[5])
+	z2 += uint64(a[1]) * uint64(b[6])
+	z2 += uint64(a[0]) * uint64(b[7])
 
-	z2 += uint64(a[7-1]) * uint64(b[1])
-	z1 += uint64(aa[7-1]) * uint64(bb[1])
-	z0 += uint64(a[8+7-1]) * uint64(b[8+1])
-
-	z2 += uint64(a[7-2]) * uint64(b[2])
-	z1 += uint64(aa[7-2]) * uint64(bb[2])
-	z0 += uint64(a[8+7-2]) * uint64(b[8+2])
-
-	z2 += uint64(a[7-3]) * uint64(b[3])
-	z1 += uint64(aa[7-3]) * uint64(bb[3])
-	z0 += uint64(a[8+7-3]) * uint64(b[8+3])
-
-	z2 += uint64(a[7-4]) * uint64(b[4])
-	z1 += uint64(aa[7-4]) * uint64(bb[4])
-	z0 += uint64(a[8+7-4]) * uint64(b[8+4])
-
-	z2 += uint64(a[7-5]) * uint64(b[5])
-	z1 += uint64(aa[7-5]) * uint64(bb[5])
-	z0 += uint64(a[8+7-5]) * uint64(b[8+5])
-
-	z2 += uint64(a[7-6]) * uint64(b[6])
-	z1 += uint64(aa[7-6]) * uint64(bb[6])
-	z0 += uint64(a[8+7-6]) * uint64(b[8+6])
-
-	z2 += uint64(a[7-7]) * uint64(b[7])
-	z1 += uint64(aa[7-7]) * uint64(bb[7])
-	z0 += uint64(a[8+7-7]) * uint64(b[8+7])
-
+	z1 += aa[7] * bb[0]
+	z1 += aa[6] * bb[1]
+	z1 += aa[5] * bb[2]
+	z1 += aa[4] * bb[3]
+	z1 += aa[3] * bb[4]
+	z1 += aa[2] * bb[5]
+	z1 += aa[1] * bb[6]
+	z1 += aa[0] * bb[7]
 	z1 -= z2
-	z0 += z2
-	z2 = 0
 
+	z0 += uint64(a[15]) * uint64(b[8])
+	z0 += uint64(a[14]) * uint64(b[9])
+	z0 += uint64(a[13]) * uint64(b[10])
+	z0 += uint64(a[12]) * uint64(b[11])
+	z0 += uint64(a[11]) * uint64(b[12])
+	z0 += uint64(a[10]) * uint64(b[13])
+	z0 += uint64(a[9]) * uint64(b[14])
+	z0 += uint64(a[8]) * uint64(b[15])
+	z0 += z2
+
+	z2 = 0
 	z1 += z2
 	z0 += z2
 
 	c[7] = limb(z0) & radixMask
-	c[7+8] = limb(z1) & radixMask
+	c[15] = limb(z1) & radixMask
 
 	z0 >>= 28
 	z1 >>= 28
