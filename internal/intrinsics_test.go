fdd254a2c4340ef48d9afc7338c813f0fb0d7631
Sofía Celi <cherenkovd69@gmail.com>
2019-12-12 14:57:39 -0500

    Add more tests

--- internal/intrinsics_test.go
+++ internal/intrinsics_test.go
@@ -4,10 +4,21 @@ import (
 	. "gopkg.in/check.v1"
 )
 
+func (s *Ed448InternalSuite) Test_WideMul32(c *C) {
+	a := uint32(0x01)
+	b := uint32(0x02)
+	n := widemul32(a, b)
+	c.Assert(n, Equals, uint64(0x02))
+
+	d := uint32(0xff)
+	n = widemul32(a, d)
+	c.Assert(n, Equals, uint64(0xff))
+}
+
 func (s *Ed448InternalSuite) Test_IsWordZero(c *C) {
-	a := uint32(0x0)
+	a := uint32(0x00)
 	ret := isWord32Zero(a)
-	c.Assert(ret, Equals, uint32(0x0))
+	c.Assert(ret, Equals, uint32(0x00))
 
 	b := uint32(0x01)
 	ret = isWord32Zero(b)
