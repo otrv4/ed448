c17a7464f774d2dcd9c12eae53dea637458acb78
Sofía Celi <cherenkovd69@gmail.com>
2020-01-17 13:37:23 -0500

    Add edwards params

--- elliptic.go
+++ elliptic.go
@@ -7,12 +7,24 @@ import (
 
 // CurveParams contains the parameters of an elliptic curve and also provides
 // a generic, non-constant time implementation of Curve.
-// This are the Montgomery params
+// These are the Montgomery params.
 type CurveParams struct {
-	P       *big.Int // the order of the underlying field
-	N       *big.Int // the order of the base point
-	A       *big.Int // the constant of the curve equation
-	Gu, Gv  *big.Int // (x,y) of the base point
+	P       *big.Int // the order of the underlying finite field
+	N       *big.Int // the prime order of the base point
+	A       *big.Int // the coeficient
+	Gu, Gv  *big.Int // (u,v) of the base point
+	BitSize int      // the size of the underlying field
+	Name    string   // the canonical name of the curve
+}
+
+// EdwardsCurveParams contains the parameters of an elliptic curve and also provides
+// a generic, non-constant time implementation of Curve.
+// These are the Edwards params.
+type EdwardsCurveParams struct {
+	P       *big.Int // the order of the underlying finite field
+	N       *big.Int // the prime order of the base point
+	D       *big.Int // the non-zero element
+	Gx, Gy  *big.Int // (x,y) of the base point
 	BitSize int      // the size of the underlying field
 	Name    string   // the canonical name of the curve
 }
@@ -23,7 +35,7 @@ type GoldilocksCurve interface {
 	// Params returns the parameters for the curve.
 	Params() *CurveParams
 	// Params returns the edwards parameters for the curve.
-	EdwardsParams() *CurveParams
+	EdwardsParams() *EdwardsCurveParams
 	// IsOnCurve reports whether the given (x,y) lies on the curve.
 	IsOnCurve(x, y *big.Int) bool
 	// Add returns the sum of (x1,y1) and (x2,y2)
@@ -44,11 +56,11 @@ func (curve *CurveParams) Params() *CurveParams {
 
 var initonce sync.Once
 var curve448 *CurveParams
-var ed448 *CurveParams
+var ed448 *EdwardsCurveParams
 
 func initAll() {
 	initCurve448()
-	//inited448()
+	initEd448()
 }
 
 func initCurve448() {
@@ -60,4 +72,15 @@ func initCurve448() {
 	curve448.Gu, _ = new(big.Int).SetString("5", 10)
 	curve448.Gv, _ = new(big.Int).SetString("3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f", 10)
 	curve448.BitSize = 448
+}
+
+func initEd448() {
+	// See https://safecurves.cr.yp.to/field.html and https://tools.ietf.org/html/rfc7748#section-4.2
+	ed448 = &EdwardsCurveParams{Name: "ed-448"}
+	ed448.P, _ = new(big.Int).SetString("726838724295606890549323807888004534353641360687318060281490199180612328166730772686396383698676545930088884461843637361053498018365439", 10)
+	ed448.N, _ = new(big.Int).SetString("181709681073901722637330951972001133588410340171829515070372549795146003961539585716195755291692375963310293709091662304773755859649779", 10)
+	ed448.D, _ = new(big.Int).SetString("-39081", 10)
+	ed448.Gx, _ = new(big.Int).SetString("224580040295924300187604334099896036246789641632564134246125461686950415467406032909029192869357953282578032075146446173674602635247710", 10)
+	ed448.Gy, _ = new(big.Int).SetString("298819210078481492676017930443930673437544040154080242095928241372331506189835876003536878655418784733982303233503462500531545062832660", 10)
+	ed448.BitSize = 448
 }
